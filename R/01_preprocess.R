9ac69c6eae8ba86271fd012753db250fcb4b6ceb
shdam <soren.helweg@gmail.com>
2020-08-31 14:07:52 +0200

    renv

--- R/01_preprocess.R
+++ R/01_preprocess.R
@@ -1,16 +1,27 @@
 #### Data pre-processing ####
 
-# Load data - the loaded contains objects "fcs_raw", "sample_ids", 
-# "batch_ids", "all_markers", which are the raw flowSet, sample ids per row, batch 
+# Load data - the loaded contains objects "fcs_raw", "sample_ids",
+# "batch_ids", "all_markers", which are the raw flowSet, sample ids per row, batch
 # ids per row and all measured markers in the panel
-preprocess <- function(fcs_raw){
+
+
+#' Transform data using asinh
+#'
+#'
+#'
+#'
+#' @importFrom flowCore parameters fsApply
+#' @importFrom Biobase pData exprs
+#' @import magrittr
+#' @family preprocess
+transform_asinh <- function(fcs_raw){
   # De-randomize and transform data using asinh
-  panel_fcs <- fcs_raw[[1]] %>% 
-    flowCore::parameters() %>% 
+  panel_fcs <- fcs_raw[[1]] %>%
+    flowCore::parameters() %>%
     Biobase::pData()
-  
+
   panel_fcs$desc <- gsub(' ', '', gsub('-', '', gsub("\\d+[A-Za-z]+_", "", panel_fcs$desc)))
-  
+
   fcs <- flowCore::fsApply(fcs_raw, function(x, cofactor = 5){
     colnames(x) <- panel_fcs$desc
     expr <- Biobase::exprs(x)
@@ -20,21 +31,12 @@ preprocess <- function(fcs_raw){
   })
   return(fcs)
 }
-if(FALSE){
-  # Load raw data
-  load("data/raw/DFCI_panel1_data.Rdata")
-  # Extract objects
-  fcs_raw <- panel1_data$fcs_raw
-  all_markers <- panel1_data$all_markers
-  sample_ids <- panel1_data$sample_ids
-  batch_ids <- panel1_data$batch_ids
-  
-  
-  # Extract expression data
-  combined_expr <- preprocess(fcs_raw) %>% 
-    flowCore::fsApply(exprs)
-}
 
+#' Create subsample of combined expression matrix
+#'
+#'
+#' @importFrom tibble as_tibble
+#' @importFrom dplyr mutate
 create_sample <- function(combined_expr,
                           batch_ids,
                           sample_ids,
@@ -42,24 +44,35 @@ create_sample <- function(combined_expr,
                           seed = 473){
   set.seed(seed)
   sample <- sample(1:nrow(combined_expr), 100000)
-  combined_expr <- combined_expr[sample,] %>% 
-    tibble::as_tibble() %>% 
+  combined_expr <- combined_expr[sample,] %>%
+    tibble::as_tibble() %>%
     mutate(batch_ids = batch_ids[sample],
            sample_ids = sample_ids[sample])
   return(combined_expr)
 }
-# Plot of distributions - downsampled to avoid problems with memory
-# set.seed(473)
-# sample <- sample(1:nrow(combined_expr), 100000)
-# combined_expr <- combined_expr[sample,] %>% 
-#   tibble::as_tibble() %>% 
-#   mutate(batch_ids = batch_ids[sample],
-#          sample_ids = sample_ids[sample])
-# batch_ids <- batch_ids[sample]
-# sample_ids <- sample_ids[sample]
-if(FALSE){
-combined_expr <- create_sample(combined_expr,
-                               batch_ids,
-                               sample_ids)
-save(combined_expr, all_markers, file = "data/01_preprocessed_data.Rdata")
+
+#'
+preprocess <- function(input,
+                       sample_size = 100000,
+                       seed = 473){
+  # Extract objects
+  fcs_raw <- input$fcs_raw
+  all_markers <- input$all_markers
+  sample_ids <- input$sample_ids
+  batch_ids <- input$batch_ids
+
+  #
+  combined_expr <- fcs_raw %>%
+    transform_asinh() %>%
+    flowCore::fsApply(exprs) %>%            # Extract expression data
+    create_sample(batch_ids = batch_ids,
+                  sample_ids = sample_ids,
+                  sample_size = sample_size,
+                  seed = seed)
+
+  return(list("data" = combined_expr,
+              "markers" = all_markers))
 }
+
+
+
--- R/utils_plotting.R
+++ R/utils_plotting.R
@@ -2,61 +2,61 @@
 
 # Density ridges for two sets
 density_plots <- function(uncorrected, corrected, batch_ids, uncorrected_order = as.factor(batch_ids), corrected_order = as.factor(batch_ids), filename) {
-  
+
   # Extract data into dataframe format
   uncor_df <- cbind.data.frame(uncorrected, uncorrected_order, rep('Uncorrected', nrow(uncorrected)))
   cor_df <- cbind.data.frame(corrected, corrected_order, rep('Corrected', nrow(corrected)))
   colnames(cor_df)[(ncol(cor_df)-1):ncol(cor_df)] <- colnames(uncor_df)[(ncol(uncor_df)-1):ncol(uncor_df)] <- c('Batch', 'Type')
-  
+
   df <- rbind.data.frame(uncor_df, cor_df)
-  
+
   # For each marker, make the plot
   p <- list()
   for (c in 1:length(all_markers)) {
-    
+
     p[[c]] <- ggplot(df, aes_string(x = all_markers[c], y = "Batch")) +
       geom_density_ridges(aes(color = Type, fill = Type), alpha = 0.4) +
       theme_bw()
   }
-  
+
   # Save the plots
   save_plot(filename, plot_grid(plotlist = p, ncol = 6), base_width = 28, base_height = 40)
-  
+
 }
 
 
 
 # Dimensionality reduction plot
 dimred_plot <- function(data, batch_ids, name, type = 'pca', plot = 'batch', marker = NULL) {
-  
+
   if (type == 'pca') {
     # Run PCA
-    pca <- data %>% 
-      select_if(is.numeric) %>% 
+    pca <- data %>%
+      select_if(is.numeric) %>%
       prcomp(scale. = TRUE, center = TRUE)
-    
+
     # Make dataframe with output
     if (plot == 'batch') {
       df <- pca$x %>%
-        as_tibble() %>% 
+        as_tibble() %>%
         mutate(Batch = as.factor(batch_ids))
       #cbind.data.frame(pca$x, as.factor(batch_ids)); colnames(df)[ncol(df)] <- 'Batch'
     } else {
       df <- cbind.data.frame(pca$x, as.factor(batch_ids), data[,marker]); colnames(df)[(ncol(df)-1):ncol(df)] <- c('Batch', marker)
     }
-    
+
   } else if (type == 'umap') {
     # Run UMAP
     set.seed(758)
-    umap <- data %>% 
-      select_if(is.numeric) %>% 
+    umap <- data %>%
+      select_if(is.numeric) %>%
       umap(n_neighbors = 15, min_dist = 0.2, metric = 'euclidean')
-    
+
     # Make dataframe with output
     if (plot == 'batch') {
-      df <- umap %>% 
-        as_tibble() %>% 
-        mutate(Batch = as.factor(batch_ids)) %>% 
+      df <- umap %>%
+        as_tibble() %>%
+        mutate(Batch = as.factor(batch_ids)) %>%
         rename(UMAP1 = V1,
                UMAP2 = V2)
       # cbind.data.frame(umap, as.factor(batch_ids)); colnames(df) <- c('UMAP1', 'UMAP2', 'Batch')
@@ -64,10 +64,10 @@ dimred_plot <- function(data, batch_ids, name, type = 'pca', plot = 'batch', mar
       df <- cbind.data.frame(umap, as.factor(batch_ids), data[,marker]); colnames(df) <- c('UMAP1', 'UMAP2', 'Batch', marker)
     }
   }
-  
+
   # Make the plot
   if (plot == 'batch') {
-    plot <- df %>% 
+    plot <- df %>%
       ggplot(aes_string(x = colnames(df)[1], y = colnames(df)[2])) +
       geom_point(aes_string(color = "Batch"), alpha = 0.3, size = 0.4) +
       guides(color = guide_legend(override.aes = list(alpha = 1, size = 1))) +
@@ -75,12 +75,12 @@ dimred_plot <- function(data, batch_ids, name, type = 'pca', plot = 'batch', mar
       ggtitle(paste(toupper(type), '-', name))
   } else {
     plot <- ggplot(df, aes_string(x = colnames(df)[1], y = colnames(df)[2])) +
-      geom_point(aes_string(color = marker), alpha = 0.3, size = 0.4) + 
+      geom_point(aes_string(color = marker), alpha = 0.3, size = 0.4) +
       #guides(colour = guide_legend(override.aes = list(alpha = 1, size = 1))) +
       theme_bw() + theme(plot.title = element_text(hjust = 0.5)) +
       ggtitle(paste(toupper(type), '-', name))
   }
-  
+
   return(plot)
 }
 
