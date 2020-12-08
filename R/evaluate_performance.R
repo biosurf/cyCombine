517f078593094314c0a8907e03ce4b6eccf554f8
shdam <soren.helweg@gmail.com>
2020-12-08 15:29:31 +0100

    Revert violin plot

--- R/evaluate_performance.R
+++ R/evaluate_performance.R
@@ -181,7 +181,7 @@ evaluate_emd <- function(preprocessed,
     plyr::round_any(5, f = ceiling) + 1
 
   # Create violin plots
-  violin <- emds %>%
+  violin <- emds_filtered %>%
     tidyr::pivot_longer(cols = ends_with("orrected"), names_to = "corrected") %>%
     ggplot(aes(x = corrected, y = value)) +
     geom_violin() +
